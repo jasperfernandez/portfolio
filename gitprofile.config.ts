67fd64cbc00e73e86dbed15ac985705a9704f844
Jasper Fernandez <jasperfernandez148@gmail.com>
2024-09-18 18:51:31 +0800

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -170,7 +170,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'lofi',
+    defaultTheme: 'dim',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
